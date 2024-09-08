cc9285f00a020390fc8af8e3fadd970587476e5b
Joseph Ruan <63382739+Ruan704@users.noreply.github.com>
2024-09-08 16:47:25 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+// App.js
+
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <img src="asserts/myphoto.JPG" height="250px" width="250px"/>
+      <h1>Joseph Ruan Xing Qi</h1>
+      <h2>Full stack developer</h2>
+      <div class="card">
+        <h3>About Me</h3>
+        <hr/>
+        <p>I am an an fresh graduate from university of Essex and looking to develop my ownskill by using from what I learn from SCTP</p>
+      </div>
+      <div class="card">
+        <h3>Contact me</h3>
+        <hr/>
+        <p>Email: xingqjoseph@gmail.com  <br/> LinkedIn: https://www.linkedin.com/in/josephruanxingqi/</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
